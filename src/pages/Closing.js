0e0f09616b5f325abc0bfbbd18c70759c53799d6
Galangyp <galangy11@gmail.com>
2023-07-21 08:44:22 +0700

    fix but beranda

--- src/pages/Closing.js
+++ src/pages/Closing.js
@@ -3,35 +3,50 @@ import { Link } from "react-router-dom";
 import "./pages.css";
 
 const Closing = () => {
+  function refresh() {
+    window.location.reload();
+  }
 
-    function refresh(){
-        window.location.reload(); 
-    }
+  return (
+    <div className="closing">
+      <h1 className="closing-selesai">SELESAI</h1>
+      <div className="closing-button">
+        <motion.div
+          className="baca-lagi"
+          whileHover={{ scale: 1, y: [null, -10], backgroundColor: "#2780c9" }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ ease: "linear", duration: 0.2 }}
+        >
+          <p
+            className="tombol-baca-lagi"
+            onClick={refresh}
+            style={{ color: "white", padding: "10px" }}
+          >
+            Baca Lagi
+          </p>
+        </motion.div>
+        <motion.div
+          className="baca-lagi"
+          style={{ marginLeft: "320px", backgroundColor: "#1c5e94" }}
+          whileHover={{ scale: 1, y: [null, -10], backgroundColor: "#2780c9" }}
+          whileTap={{ scale: 0.9 }}
+          transition={{ ease: "linear", duration: 0.2 }}
+        >
+          <Link
+            to="/"
+            style={{
+              textDecoration: "none",
+              color: "white",
+            }}
+          >
+            <p className="tombol-baca-lagi" style={{ padding: "10px" }}>
+              Beranda
+            </p>
+          </Link>
+        </motion.div>
+      </div>
+    </div>
+  );
+};
 
-    return ( 
-        <div className="closing">
-            <h1 className="closing-selesai">SELESAI</h1>
-            <div className="closing-button">
-                <motion.div
-                    className="baca-lagi"
-                    whileHover={{ scale: 1, y:[null, -10], backgroundColor: '#2780c9' }}
-                    whileTap={{ scale:0.9 }}
-                    transition={{ ease:"linear", duration:0.2 }}
-                >
-                    <p className="tombol-baca-lagi" onClick={ refresh } style={{ color:"white", padding:"10px" }}>Baca Lagi</p>              
-                </motion.div>
-                <motion.div
-                    className="beranda"
-                    style={{ marginLeft:'320px' }}
-                    whileHover={{ scale: 1, y:[null, -10], backgroundColor: '#2780c9' }}
-                    whileTap={{ scale:0.9 }}
-                    transition={{ ease:"linear", duration:0.2 }}
-                >
-                    <p className="beranda-closing" style={{ padding:"10px" }}><Link to="/" style={{ textDecoration:"none", color:"white"}}>Beranda</Link></p>  
-                </motion.div>
-            </div>
-        </div>
-     ); 
-}
- 
 export default Closing;
